8188be105d87a85b931c852dd4d26ecccf40269a
kreativsahara <technologieslensmen@gmail.com>
2026-02-18 20:34:31 +0300

    feat: add Admin Dashboard and various button components for user actions

---
+++ src/App/admin/components/button/btnBeSeller.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from 'react-router-dom'
+
+function btnBeSeller() {
+    const navigate = useNavigate()
+    const UpgradeUser = async () => {
+        navigate('/become-seller', { replace: true })
+    }
+    return (
+        <button className='bg-gray-950 text-white rounded-md  px-3 md:px-4 py-2 text-sm font-medium hover:bg-gray-800 transition duration-300' onClick={UpgradeUser}>
+            Become a Seller
+        </button>
+    )
+}
+
+export default btnBeSeller
