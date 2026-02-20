6cf75af7e8bc813a9bd00959788b389a9d94b54e
kreativsahara <technologieslensmen@gmail.com>
2026-02-20 19:03:59 +0300

    git: stop ignoring src/api/axios.js by commenting out its entry in .gitignore.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'https://backend.diksxcars.co.ke' || import.meta.env.VITE_API_URL;
-// const BASE_URL = 'http://localhost:4000';
+// const BASE_URL = 'https://backend.diksxcars.co.ke' || import.meta.env.VITE_API_URL;
+const BASE_URL = 'http://localhost:4000';
 
 export default axios.create({
     baseURL: BASE_URL 
